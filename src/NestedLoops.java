b952c2314d4827ff672c6d6b730eb5e1c872bc75
Christian Palombo <palombochristian21@gmail.com>
2021-10-07 23:07:16 -0400

    Second Commit

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -6,53 +6,57 @@ public class NestedLoops {
 
     public static void main(String[] args) {
 
+        int dice1 = 0;
+        int dice2 = 0;
+        int dice3 = 0;
+        int dice4 = 0;
+        int dice5 = 0;
+        int dice6 = 0;
         Random rand = new Random();
-        int count;
-        int diceSet = 0;
-        Scanner input = new Scanner(System.in);
-        String askAgain;
-        do {
 
-            for (count = 1; count <= 6; count++) {
-                int diceRoll = rand.nextInt(7);
-                diceSet++;
-                if (count == 1) {
-                    if (diceRoll == 1) {
-                        System.out.println("You first roll was equal to a: " + diceRoll);
-                    }
-                }
-                if (count == 2) {
-                    if (diceRoll == 2) {
-                        System.out.println("Your second roll was equal to a:  " + diceRoll);
-                    }
-                }
-                if (count == 3) {
-                    if (diceRoll == 3) {
-                        System.out.println("Your third roll was equal to a:  " + diceRoll);
-                    }
-                }
-                if (count == 4) {
-                    if (diceRoll == 4) {
-                        System.out.println("Your fourth roll was equal to a:  " + diceRoll);
-                    }
-                }
-                if (count == 5) {
-                    if (diceRoll == 5) {
-                        System.out.println("Your fifth roll was equal to a:  " + diceRoll);
-                    }
-                }
-                if (count == 6) {
-                    if (diceRoll == 6) {
-                        System.out.println("Your sixth roll is a equal to a:  " + diceRoll);
-                    }
-                }
 
+        boolean isDone = true;
+        int count = 0;
+        while (isDone) {
+            count++;
+
+
+            for (int index = 0; index <= 5; index++) {
+                if (index == 0) {
+                    dice1 = rand.nextInt(6);
+
+                } else if (index == 1) {
+                    dice2 = rand.nextInt(6);
+
+                } else if (index == 2) {
+                    dice3 = rand.nextInt(6);
+
+                } else if (index == 3) {
+                    dice4 = rand.nextInt(6);
+
+                } else if (index == 4) {
+                    dice5 = rand.nextInt(6);
+
+
+                } else if (index == 5) {
+                    dice6 = rand.nextInt(6);
+
+                }
             }
-            System.out.println("Would you like to roll again? Y for (YES). Enter any other letter to exit program when dice roll sequence has been achieved!" +
-                               "\nIf you roll 1,2,3 etc in a row. Your result will be displayed.");
-            askAgain = input.nextLine();
-        } while (askAgain.equalsIgnoreCase("y"));
-        System.out.println("It took " + diceSet + " times to get all dice to be rolled in a row");
+
+            if (dice1 == 1 && dice2 == 1 && dice3 == 2 && dice4 == 2 && dice5 == 3 && dice6 == 3) {
+                System.out.println(dice1);
+                System.out.println(dice2);
+                System.out.println(dice3);
+                System.out.println(dice4);
+                System.out.println(dice5);
+                System.out.println(dice6);
+                System.out.println("The loop took " + count + " times");
+                isDone = false;
+            }
+
+
+        }
 
 
     }
